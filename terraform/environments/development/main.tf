0905fd3125c1977a545599d197be8b924386b6f9
AmithSAI007 <amith@dataarize.com>
2026-03-25 17:16:20 +0530

    fix: fix symlink for modules

--- terraform/environments/development/main.tf
+++ terraform/environments/development/main.tf
@@ -3,18 +3,18 @@ module "storage" {
 }
 
 module "pubsub" {
-  source = "./modules/pubsub"
+  source = "../../modules/pubsub"
 }
 
 module "service" {
-  source                      = "../../modules/services"
+  source                      = "../../modules/service"
   project_id                  = var.project_id
   project_region              = var.project_region
   container_image             = var.container_image
   service_account_name        = var.service_account_name
   memory_limit                = var.memory_limit
   cpu_limit                   = var.cpu_limit
-  gcs_bucket                  = module.storage.raw_videos_bucket_name
+  gcs_bucket                  = module.storage.processed_videos_bucket_name
   pubsub_subscription_id      = module.pubsub.apex_transcoder_subscription_id
   min_instance_count          = var.min_instance_count
   max_instance_count          = var.max_instance_count
